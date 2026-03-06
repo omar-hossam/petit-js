005010ce0515b1976789800363326738a7d34acd
omar-hossam <omarhossam50@proton.me>
2026-03-06 17:01:11 +0200

    implement class, attributes handlers

---
+++ x.js
@@ -0,0 +1,45 @@
+// CLASS
+function togClass(el, val) {
+  if (typeof(el) === "string") {
+    [...document.querySelectorAll(el)].forEach(e => e.classList.toggle(val))
+    return;
+  }
+  el.classList.toggle(val)
+}
+
+function addClass(el, val) {
+  if (typeof(el) === "string") {
+    [...document.querySelectorAll(el)].forEach(e => e.classList.add(val))
+    return;
+  }
+  el.classList.add(val)
+}
+
+function remClass(el, val) {
+  if (typeof(el) === "string") {
+    [...document.querySelectorAll(el)].forEach(e => e.classList.remove(val))
+    return;
+  }
+  el.classList.remove(val)
+}
+
+// ATTRIBUTES
+function togAttr(el, attr) {
+  if (typeof(el) === "string") {
+    [...document.querySelectorAll(el)].forEach(e => e.toggleAttribute(attr))
+    return;
+  }
+  el.toggleAttribute(attr)
+}
+
+function setAttr(el, attr, val) {
+  if (typeof(el) === "string") {
+    [...document.querySelectorAll(el)].forEach(e => e.setAttribute(attr, val))
+    return;
+  }
+  el.toggleAttribute(attr, val)
+}
+
+function remAttr(el, attr) {
+  setAttr(el, attr, "")
+}
