f39bacdaace2c350502d3f21de1c660810f50d1c
omar-hossam <omarhossam50@proton.me>
2026-03-07 10:08:27 +0200

    implement & refactor h-for

--- x.js
+++ x.js
@@ -50,55 +50,41 @@ TODOS:
 - [X] Classes
 - [X] Attributes
 
-- [ ] data-for
-- [ ] data-if, data-elseif, data-else
-- [ ] data-text (= reactive to variable value) loads TEXT ONLY NOT HTML!
-- [ ] data-while
+- [ ] h-for
+- [ ] h-if, h-elseif, h-else
+- [ ] h-text (= reactive to variable value) loads TEXT ONLY NOT HTML!
+- [ ] h-while
 
 */
 
 
 // APP OBJECTS MUST START WITH 'my' e.g. 'myApp', 'myProgram', 'mySystem' etc..
 const appObjects = Object.keys(window).filter(ob => ob.startsWith("my"));
-const allDocumentEls = [...document.querySelectorAll("body *")].filter(el => el.tagName.toLowerCase() !== "script");
+const forElements = [...document.querySelectorAll("[h-for]")];
 
 
 const isNumber = (str) => !isNaN(str) && !isNaN(parseFloat(str));
 
 
-allDocumentEls.forEach(documentEl => {
-  // data-* will go here..
-  let shareVariablesScope = false;
+forElements.forEach(el => {
+  const value = el.getAttribute("h-for").split("in").map(str => str.trim()); // split by in, remove whitespaces from start and end only
+  const children = [...el.children];
+  el.innerHTML = '';
+  const elObj = { 
+    name: value[1].split('.')[0],
+    varName: value[1].split('.')[1] 
+  }
+  // hobby in hobbies 
+  // -- here the value of `hobbies` is the `targetValue`
+  const targetValue = window[elObj.name][elObj.varName]
   
-  if (documentEl.getAttribute("data-for")) {
-    shareVariablesScope = true;
-    const dataForVal = documentEl.getAttribute("data-for").split("in").map(str => str.trim()); // split by in, remove whitespaces from start and end only
-    
-    let count = 0;
-    const children = [...documentEl.children];
-    
-    documentEl.innerHTML = ``; // we will render it later on..
-    
-    const elObj = { 
-      name: dataForVal[1].split('.')[0],
-      varName: dataForVal[1].split('.')[1] 
-    }
-    
-    // hobby in hobbies 
-    // -- here the value of `hobbies` is the `targetValue`
-    const targetValue = window[elObj.name][elObj.varName]
-    
-    log(targetValue)
-    
-    // NOW render the new children
-    for (let i = 0; i < targetValue.length; i++) {
-      children.forEach(child => {
-        if (child.getAttribute("data-text")) {
-          // now we will set the data-text generated value
-        }
-        
-        documentEl.innerHTML += child.outerHTML;
-      })
-    }
+  for (let i = 0; i < targetValue.length; i++) {
+    children.forEach(child => {
+      if (child.getAttribute("data-text")) {
+        // now we will set the data-text generated value
+      }
+      
+      el.innerHTML += child.outerHTML;
+    })
   }
 })
