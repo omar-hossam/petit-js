60cf5da3632a4565c5a9c34705232b483997f4a5
omar-hossam <omarhossam50@proton.me>
2026-03-09 00:35:21 +0200

    refactor: add the entire framework inside DOMContentLoaded

--- hilal.js
+++ hilal.js
@@ -1,157 +1,143 @@
-// CLASS
-function togClass(el, val) {
-  if (typeof(el) === "string") {
-    [...document.querySelectorAll(el)].forEach(e => e.classList.toggle(val))
-    return;
-  }
-  el.classList.toggle(val)
-}
-
-function addClass(el, val) {
-  if (typeof(el) === "string") {
-    [...document.querySelectorAll(el)].forEach(e => e.classList.add(val))
-    return;
-  }
-  el.classList.add(val)
-}
-
-function remClass(el, val) {
-  if (typeof(el) === "string") {
-    [...document.querySelectorAll(el)].forEach(e => e.classList.remove(val))
-    return;
-  }
-  el.classList.remove(val)
-}
-
-// ATTRIBUTES
-function togAttr(el, attr) {
-  if (typeof(el) === "string") {
-    [...document.querySelectorAll(el)].forEach(e => e.toggleAttribute(attr))
-    return;
-  }
-  el.toggleAttribute(attr)
-}
-
-function setAttr(el, attr, val) {
-  if (typeof(el) === "string") {
-    [...document.querySelectorAll(el)].forEach(e => e.setAttribute(attr, val))
-    return;
-  }
-  el.toggleAttribute(attr, val)
-}
-
-function log(val) { console.log(val) }
-
-/*
-TODOS:
-
-## Functions
-
-- [X] Classes
-- [X] Attributes
-
-- [ ] h-for
-- [ ] h-if, h-elseif, h-else
-- [ ] h-text (= reactive to variable value) loads TEXT ONLY NOT HTML!
-- [ ] h-while
-
-*/
-
-
-// APP OBJECTS MUST START WITH 'my' e.g. 'myApp', 'myProgram', 'mySystem' etc..
-const appObjects = Object.keys(window).filter(ob => ob.startsWith("my"));
-const forElements = [...document.querySelectorAll("[h-for]")];
-
-const textElements = [...document.querySelectorAll("[h-text]")];
-
-let alreadyTrackedHText = [];
-
-const isNumber = (str) => !isNaN(str) && !isNaN(parseFloat(str));
-
-function setElText(el, forArrayValue="", forArrayVarName="") {
-  if (forArrayValue) {
-    let hText = el.getAttribute("h-text")
-    let result = hText.replace(forArrayVarName, forArrayValue).replaceAll("'", "").replaceAll('"', "")
-    
-    el.textContent = result;
-    return;
-  }
-  let hText = el.getAttribute("h-text").split("").map(str => str.trim()).filter(char => char !== "");
-  let final = "";
-  let operators = ["+", "-", "*", "/", "%", "**"] 
-  
-  let variables = []
-  let varName = ""
-  
-  for (let i = 0; i < hText.length; i++) {
-    if (!isNumber(hText[i]) && !operators.includes(hText[i])) { 
-      varName += hText[i]
-    } else { 
-      if (varName) final += getVariableValue(varName); varName = ""; 
-      final += hText[i]
+document.addEventListener('DOMContentLoaded', () => {
+  // CLASS
+  function togClass(el, val) {
+    if (typeof(el) === "string") {
+      [...document.querySelectorAll(el)].forEach(e => e.classList.toggle(val))
+      return;
     }
-
+    el.classList.toggle(val)
   }
-  if (varName) final += getVariableValue(varName);
-  if (!isNumber(final[0]) && !isNumber(final[final.length-1])) {
-    el.textContent = final
-    console.log(el.outerHTML)
-  } else {
-    el.textContent = new Function("return " + final)();
-  }
-}
 
-function getVariableValue(varName) {
-  if (varName.includes(".") 
-      && appObjects.includes(varName.split('.')[0])
-  ) {
-    return window[varName.split('.')[0]][varName.split('.')[1]]
+  function addClass(el, val) {
+    if (typeof(el) === "string") {
+      [...document.querySelectorAll(el)].forEach(e => e.classList.add(val))
+      return;
+    }
+    el.classList.add(val)
   }
-}
 
-forElements.forEach(el => {
-  const value = el.getAttribute("h-for").split("in").map(str => str.trim()); // split by in, remove whitespaces from start and end only
-  const children = [...el.children];
-  el.innerHTML = '';
-  
-  if (isNumber(value[0])) { 
+  function remClass(el, val) {
+    if (typeof(el) === "string") {
+      [...document.querySelectorAll(el)].forEach(e => e.classList.remove(val))
+      return;
+    }
+    el.classList.remove(val)
+  }
+
+  // ATTRIBUTES
+  function togAttr(el, attr) {
+    if (typeof(el) === "string") {
+      [...document.querySelectorAll(el)].forEach(e => e.toggleAttribute(attr))
+      return;
+    }
+    el.toggleAttribute(attr)
+  }
+
+  function setAttr(el, attr, val) {
+    if (typeof(el) === "string") {
+      [...document.querySelectorAll(el)].forEach(e => e.setAttribute(attr, val))
+      return;
+    }
+    el.toggleAttribute(attr, val)
+  }
+
+  function log(val) { console.log(val) }
+
+  // APP OBJECTS MUST START WITH 'my' e.g. 'myApp', 'myProgram', 'mySystem' etc..
+  const appObjects = Object.keys(window).filter(ob => ob.startsWith("my"));
+  const forElements = [...document.querySelectorAll("[h-for]")];
+
+  const textElements = [...document.querySelectorAll("[h-text]")];
+
+  let alreadyTrackedHText = [];
+
+  const isNumber = (str) => !isNaN(str) && !isNaN(parseFloat(str));
+
+  function setElText(el, forArrayValue="", forArrayVarName="") {
+    if (forArrayValue) {
+      let hText = el.getAttribute("h-text")
+      let result = hText.replace(forArrayVarName, forArrayValue).replaceAll("'", "").replaceAll('"', "")
+      
+      el.textContent = result;
+      return;
+    }
+    let hText = el.getAttribute("h-text").split("").map(str => str.trim()).filter(char => char !== "");
+    let final = "";
+    let operators = ["+", "-", "*", "/", "%", "**"] 
     
-    let numeralFor = []
+    let variables = []
+    let varName = ""
     
-    if (value[1].includes("by")) { // for 1 in 10 by 2
-      const arr = [...value[1].split("by")]
-      numeralFor = [...value[0], ...arr].map(s => Number(s));
+    for (let i = 0; i < hText.length; i++) {
+      if (!isNumber(hText[i]) && !operators.includes(hText[i])) { 
+        varName += hText[i]
+      } else { 
+        if (varName) final += getVariableValue(varName); varName = ""; 
+        final += hText[i]
+      }
+
+    }
+    if (varName) final += getVariableValue(varName);
+    if (!isNumber(final[0]) && !isNumber(final[final.length-1])) {
+      el.textContent = final
+      console.log(el.outerHTML)
     } else {
-      numeralFor = [...value, ..."1"].map(s => Number(s));  
-    }
-    // for 1 in 10
-    // log (numeralFor)
-    
-    for (let i = numeralFor[0]; i < numeralFor[1]; i += numeralFor[2]) {
-      children.forEach(child => {
-        el.innerHTML += child.outerHTML
-      })
-    }
-    
-    
-  } else { // loop through an array
-    const elObj = { 
-      name: value[1].split('.')[0],
-      varName: value[1].split('.')[1] 
-    }
-    // hobby in hobbies 
-    // -- here the value of `hobbies` is the `targetValue`
-    const targetValue = window[elObj.name][elObj.varName]
-    
-    log(targetValue)
-    for (let i = 0; i < targetValue.length; i++) {
-      children.forEach(child => {
-        setElText(child, forArrayValue=targetValue[i], forArrayVarName=value[0])
-        alreadyTrackedHText.push(child)
-        el.innerHTML += child.outerHTML;
-      })
+      el.textContent = new Function("return " + final)();
     }
   }
-})
 
-textElements.filter(e => !alreadyTrackedHText.includes(e)).forEach(e => setElText(e))
+  function getVariableValue(varName) {
+    if (varName.includes(".") 
+        && appObjects.includes(varName.split('.')[0])
+    ) {
+      return window[varName.split('.')[0]][varName.split('.')[1]]
+    }
+  }
+  
+  forElements.forEach(el => {
+    const value = el.getAttribute("h-for").split("in").map(str => str.trim()); // split by in, remove whitespaces from start and end only
+    const children = [...el.children];
+    el.innerHTML = '';
+    
+    if (isNumber(value[0])) { 
+      
+      let numeralFor = []
+      
+      if (value[1].includes("by")) { // for 1 in 10 by 2
+        const arr = [...value[1].split("by")]
+        numeralFor = [...value[0], ...arr].map(s => Number(s));
+      } else {
+        numeralFor = [...value, ..."1"].map(s => Number(s));  
+      }
+      // for 1 in 10
+      // log (numeralFor)
+      
+      for (let i = numeralFor[0]; i < numeralFor[1]; i += numeralFor[2]) {
+        children.forEach(child => {
+          el.innerHTML += child.outerHTML
+        })
+      }
+      
+      
+    } else { // loop through an array
+      const elObj = { 
+        name: value[1].split('.')[0],
+        varName: value[1].split('.')[1] 
+      }
+      // hobby in hobbies 
+      // -- here the value of `hobbies` is the `targetValue`
+      const targetValue = window[elObj.name][elObj.varName]
+      
+      log(targetValue)
+      for (let i = 0; i < targetValue.length; i++) {
+        children.forEach(child => {
+          setElText(child, forArrayValue=targetValue[i], forArrayVarName=value[0])
+          alreadyTrackedHText.push(child)
+          el.innerHTML += child.outerHTML;
+        })
+      }
+    }
+  })
+
+  textElements.filter(e => !alreadyTrackedHText.includes(e)).forEach(e => setElText(e))
+})
